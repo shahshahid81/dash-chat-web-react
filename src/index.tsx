eb4515141c7aea7eb965f3bddad5c9a17da71e78
Shahid Shah <shahshahid2123@gmail.com>
2023-03-04 22:44:44 +0530

    Initialize project using Create React App

---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Root />}></Route>)
+);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/routes/root.tsx
@@ -0,0 +1,3 @@
+export default function root() {
+  return <h1>Hello</h1>;
+}
